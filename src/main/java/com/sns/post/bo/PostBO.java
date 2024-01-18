11714b8b4b0b886aec8ad64ac62dfb9b2833882a
test <test@test.com>
2024-01-18 18:43:54 +0900

    feat: 이미지 업로드

--- src/main/java/com/sns/post/bo/PostBO.java
+++ src/main/java/com/sns/post/bo/PostBO.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.sns.common.FileManagerService;
 import com.sns.post.entity.PostEntity;
 import com.sns.post.repository.PostRepository;
 
@@ -14,8 +16,32 @@ public class PostBO {
 	@Autowired
 	private PostRepository postRepository;
 	
+	@Autowired
+	private FileManagerService fileManagerService;
+	
 	public List<PostEntity> getPostEntityList() {
 		return postRepository.findAllByOrderByIdDesc();
+	}
+	
+	public PostEntity addPost(
+			int userId,
+			String userLoginId,
+			String content,
+			MultipartFile file) {
+		// imagePath 초기화
+		String imagePath = null;
+		
+		// file이 있으면 경로 할당
+		if(file != null) {
+			imagePath = fileManagerService.saveFile(userLoginId, file);
+		}
+		
+		// Mapper 요청
+		return postRepository.save(PostEntity.builder()
+				.userId(userId)
+				.content(content)
+				.imagePath(imagePath)
+				.build());
 	}
 
 }
